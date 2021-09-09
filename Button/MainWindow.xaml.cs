01f02870dd341f796de3f5eb49eea238fe2e956c
Курицын Алексей <al.curitsin@gmail.com>
2021-09-09 16:51:57 +0500

    Task_1 --done Кнопки занимают 2/3 ширины окна

---
+++ Button/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
